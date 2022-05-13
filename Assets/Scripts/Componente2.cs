8c827c2204cb8c0b4a9310bfa170cd84531f5b52
Adan <adanvallepacheco@live.com.mx>
2022-05-12 21:34:08 -0500

    Orden de ejecución
    Hola desde las diferentes funciones

---
+++ Assets/Scripts/Componente2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Componente2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi, mi name is " + Componente1.miObjeto.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
