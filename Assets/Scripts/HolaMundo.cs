3906d186041b8cfc9e2ebea56a76501751ea886c
Adan <adanvallepacheco@live.com.mx>
2023-08-03 21:22:04 -0600

    Primer Script
    Script con funciones de impresión en consola

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Prueba para mensaje haciendo uso de fución print()");
+        Debug.Log("Hola Mundo");
+        Debug.LogWarning("Mensaje de advertencia");
+        Debug.LogError("Mensaje de error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
